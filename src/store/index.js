2ff44888d99708ec38a239f0cb099015b9204b24
NicolaVerlato <nicola.verlato00@gmail.com>
2023-06-23 11:28:48 +0200

    refactoring

--- src/store/index.js
+++ src/store/index.js
@@ -1,53 +1,15 @@
-import { createSlice, configureStore } from '@reduxjs/toolkit';
-import { act } from 'react-dom/test-utils';
-
-const initialCounterState = { counter: 0, showCounter: true }
-
-const counterSlice = createSlice({
-    name: 'counter',
-    initialState: initialCounterState,
-    reducers: {
-        increment(state) {
-            state.counter++
-        },
-        decrement(state) {
-            state.counter--
-        },
-        increase(state, action) {
-            state.counter = state.counter + action.payload // si usa payload perchè è di default (di react toolkit) il valore che passiamo alla funzione quando viene chiamata
-        },
-        toggleCounter(state) {
-            state.showCounter = !state.showCounter
-        },
-    }
-});
-
-const initialAuthState = {
-    isAuthenticated: false
-}
- 
-const authSlice = createSlice({
-    name: 'auth',
-    initialState: initialAuthState,
-    reducers: {
-        login(state){
-            state.isAuthenticated = true
-        },
-        logout(state){
-            state.isAuthenticated = false
-        }
-    }
-})
+import { configureStore } from '@reduxjs/toolkit';
+import counterReducer from './counter'
+import authReducer from './auth'
 
 
 const store = configureStore({
     reducer: 
     {
-        counter: counterSlice.reducer,
-        auth: authSlice.reducer
+        counter: counterReducer,
+        auth: authReducer
     }
 });
 
-export const counterActions = counterSlice.actions;
-export const authActions = authSlice.actions;
+
 export default store;
